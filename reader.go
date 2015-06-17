6cb35688c2805518b45d035e9e8a33de34382582
Lucas Stadler <lu@papill0n.org>
2015-06-17 21:20:49 +0200

    make this `go get`/importable

--- reader.go
+++ reader.go
@@ -1,7 +1,7 @@
 // References:
 //  - http://edn-format.org
 //  - https://github.com/clojure/clojure/blob/master/src/jvm/clojure/lang/EdnReader.java
-package main
+package edn
 
 import (
 	"bytes"
@@ -10,53 +10,15 @@ import (
 	"fmt"
 	"io"
 	"math/big"
-	"reflect"
 	"regexp"
 	"strconv"
 	"strings"
 	"time"
 )
 
-func main() {
-	readAndPrint("[1 2 3 :hey \"ho\"]")
-	readAndPrint("(4 5 6 yay/nay)")
-	readAndPrint("#{1 3 -7 100 3 -7 :oops}")
-	readAndPrint("{:a \"b\" :c d}")
-	readAndPrint("#inst \"1985-04-12T23:20:50.52Z\"")
-	readAndPrint("#uuid \"f81d4fae-7dec-11d0-a765-00a0c91e6bf6\"")
-	readAndPrint("[1 ;commentfree\n more]")
-	readAndPrint("[1 ;commentfree\n more")
-	readAndPrint("[1 2 3 ")
-	readAndPrint("[1 2 3")
-	readAndPrint("#_hidden 42")
-	readAndPrint("0")
-	readAndPrint("0N")
-	readAndPrint("1214")
-	readAndPrint("0xff")
-	readAndPrint("13")
-	readAndPrint("2r1111")
-	readAndPrint("2r1111N")
-	readAndPrint("3r12")
-	readAndPrint("3r12N")
-	readAndPrint("3.1415")
-	readAndPrint("0.23532e10")
-	readAndPrint("-252.346436634633")
-	readAndPrint("0.2352M")
-	readAndPrint("3/45")
-	readAndPrint("-253/9")
-	readAndPrint("4/6")
-	readAndPrint("8/2")
-}
-
-func readAndPrint(s string) {
-	buf := bytes.NewReader([]byte(s))
-
-	val, err := read(buf)
-	if err != nil {
-		val = err
-	}
-
-	fmt.Printf("%#-50v %-35v %v\n", s, reflect.TypeOf(val), val)
+func DecodeString(s string) (interface{}, error) {
+	buf := bytes.NewBuffer([]byte(s))
+	return read(buf)
 }
 
 func read(r io.ByteScanner) (interface{}, error) {
---
+++ reader_test.go
@@ -0,0 +1,50 @@
+package edn
+
+import (
+	"bytes"
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestExamples(t *testing.T) {
+	readAndPrint("[1 2 3 :hey \"ho\"]")
+	readAndPrint("(4 5 6 yay/nay)")
+	readAndPrint("#{1 3 -7 100 3 -7 :oops}")
+	readAndPrint("{:a \"b\" :c d}")
+	readAndPrint("#inst \"1985-04-12T23:20:50.52Z\"")
+	readAndPrint("#uuid \"f81d4fae-7dec-11d0-a765-00a0c91e6bf6\"")
+	readAndPrint("[1 ;commentfree\n more]")
+	readAndPrint("[1 ;commentfree\n more")
+	readAndPrint("[1 2 3 ")
+	readAndPrint("[1 2 3")
+	readAndPrint("#_hidden 42")
+	readAndPrint("0")
+	readAndPrint("0N")
+	readAndPrint("1214")
+	readAndPrint("0xff")
+	readAndPrint("13")
+	readAndPrint("2r1111")
+	readAndPrint("2r1111N")
+	readAndPrint("3r12")
+	readAndPrint("3r12N")
+	readAndPrint("3.1415")
+	readAndPrint("0.23532e10")
+	readAndPrint("-252.346436634633")
+	readAndPrint("0.2352M")
+	readAndPrint("3/45")
+	readAndPrint("-253/9")
+	readAndPrint("4/6")
+	readAndPrint("8/2")
+}
+
+func readAndPrint(s string) {
+	buf := bytes.NewReader([]byte(s))
+
+	val, err := read(buf)
+	if err != nil {
+		val = err
+	}
+
+	fmt.Printf("%#-50v %-35v %v\n", s, reflect.TypeOf(val), val)
+}
